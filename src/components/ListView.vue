7c67cdae90dd31d3460b210521e519b364284792
Rumpa047 <47315020+Rumpa047@users.noreply.github.com>
2019-02-12 16:46:43 +0600

    2nd commit
    create static data and show in server

---
+++ src/components/ListView.vue
@@ -0,0 +1,18 @@
+<template>
+  <dir>
+    <h1>hello</h1>
+    <dir v-bind:key="item.id" v-for="item in data">
+      <h3>{{item.title}}</h3>
+    </dir>
+  </dir>
+</template>
+
+<script>
+export default {
+  name: "ListView",
+  props: ["data"]
+};
+</script>
+
+<style scoped>
+</style>
